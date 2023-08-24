18b502cadfec8c8fbe1d9ceba97baad2366acff7
Kaio Fontenele <mamakaiofrep@gmail.com>
2023-08-24 12:03:04 -0300

    try except

--- Exepct_try.dart
+++ Exepct_try.dart
@@ -1,10 +1,20 @@
+import 'dart:math';
+
 void main(){
   try{
-int resultado = 100 ~/ 0;
-print(resultado);
-} catch (e) { print(e);
+    int resultado = 100 ~/ 2;
+    print(resultado);
 
-}
+
+double? valor = double.parse('50.2a');
+print(valor);
+
+} on UnsupportedError{
+  print('Deu Merda aqui ');
+
+}on FormatException {print('Caiu no format execption ${e}');
+}catch(e){
+  print(e);}
 
 print('Final');
 }
