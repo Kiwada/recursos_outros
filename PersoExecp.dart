9ee762d0c7967292e1055a1f9335ef6f8782acf0
Kaio Fontenele <mamakaiofrep@gmail.com>
2023-08-25 10:58:35 -0300

    personalizado

---
+++ PersoExecp.dart
@@ -0,0 +1,25 @@
+void main(){
+  try {  
+    funcao(1);}
+    catch (e){
+    print(e);
+  }
+
+}
+
+
+
+void funcao(int x){
+  if(x <= 0 ){
+    throw VoceEstaFazendoBesteira();
+}
+print(x);
+
+}
+
+class VoceEstaFazendoBesteira  implements Exception {
+  String toString(){ 
+    return 'Você não pode passar um valor menor igual a 0';}
+
+
+}
