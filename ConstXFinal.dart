d83f9d5d1008d06a488386f7c0cfc2cd31504b42
Kaio Fontenele <mamakaiofrep@gmail.com>
2023-08-25 12:12:00 -0300

    final e const

---
+++ ConstXFinal.dart
@@ -0,0 +1,13 @@
+void main(){
+  const nome = 'Daniel';
+  const int idade = 50 + 30;
+  const List <String> lista = ['Daniel ' , 'Ciolfii'];
+
+final String sobrenome;
+if(nome == 'Daniel'){
+  sobrenome = 'Ciolfi';
+} else {
+  sobrenome = 'Não sei';
+}
+
+}
